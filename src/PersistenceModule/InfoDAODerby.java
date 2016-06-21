2a020c4defd7af5a59de6001ac8f575ff22a72ec
SigmaCreator <mourarovane@gmail.com>
2016-06-20 21:30:52 -0300

    keikaku

--- src/PersistenceModule/InfoDAODerby.java
+++ src/PersistenceModule/InfoDAODerby.java
@@ -3,10 +3,8 @@ package PersistenceModule;
 import ExceptionModule.NonExistentInformationException;
 import OperationModule.InfoDAO;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class InfoDAODerby implements InfoDAO{ //OK
